35177def67e32733d4b0c3ed8dc33fbe0600ee94
KashF <Kashfarhadi@gmail.com>
2019-02-07 11:53:25 -0500

    added eveyrhting

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const app = express()
 app.use(express.static('public'))
 app.use(express.json())
 
+let updates = []
 // Fill in your request handlers here
+/* req.body
+        clientUpdates: Array<SetColor>
+        clientSequence: number
+    res.body
+        updates: Array<SetColor>
+        sequence: number
+    SetColor
+        Array<number, number, string>
+
+*/
+app.post('/updates', (req, res) => {
+// ...
+
+})
 
 app.listen(port)
--- public/polling.js
+++ public/polling.js
@@ -1,1 +1,30 @@
 // Add logic to this script to poll server every second for updated pixels.
+
+let clientSequence = 0    // represents where client is at regarding masterlist
+const clientUpdates = []    // everything client done in last second
+
+function pollForUpdates() {
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            clientUpdates,
+            clientSequence
+        })
+    }
+
+    //clear clientUpdates array since we don't want updates sent twice
+    clientUpdates.length = 0
+
+    // fetch in function so we can keep calling it recursively
+    fetch('/updates', requestOptions)
+        .then(res => res.json())
+        .then(res => {  //...
+
+        })
+}
+
+pollForUpdates()
